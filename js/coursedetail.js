370b356da44c2742b1c6cc6069ac6b9e06440029
graciejane713 <89928869+graciejane713@users.noreply.github.com>
2021-11-16 14:26:58 -0500

    Project1

--- js/coursedetail.js
+++ js/coursedetail.js
@@ -1,60 +1,46 @@
 
-let courses= 
-[
-  {"Department":"CSC","Number":205,"Section":1,"Title":"HCI DESIGN & PROGRAMMING","Faculty":"Madeira, Scott","Openings":10,"Capacity":25,"Status":"Open","Day":"MWF","StartTime":"11:15:00 AM","EndTime":"12:10 PM","Campus":" Main Campus","Building":" Science and Engineering","Room":" SE 341 Computer Science Lab","Credits":3,"Start Date":"8\/30\/2021","End Date":"12\/17\/2021\r\n"}
-]  
+
 window.onload = (event) => 
 {
     
-    console.log('The page is loaded.We are in the consol');
-    let filteredData = removeColumns(courses);
-    let table = document.getElementById("course-table")
-    let data = Object.keys(filteredData [0]);
-    generatePageTitle(table, data);
-    generatePage(table, filteredData);
-    var myTable = document.getElementById('course-table');
-    myTable.style.border="13px solid white";
+    let id = getCourseId();
+
+    let serverUrl = getServerUrl(id);
+
+    fetch(serverUrl)
+        .then(response => response.json())
+        .then(data => displayData(data));
 }
 
-function generatePageTitle(table, data) 
-{
-    let pgtitle = table.createTHead();
-    let row = pgtitle.insertRow();
-    for (let column of data) 
-    {
-        let th = document.createElement("th");
-        let text = document.createTextNode(column);
-        th.appendChild(text);
-        row.appendChild(th);
-    }
+function getCourseId() {
+    let url_str = document.URL;
+    let url = new URL(url_str);
+    return url.searchParams.get('id');
 }
 
-function generatePage(table, data) 
-{
-    let pgcontent = table.createTBody();
-    for (let course of data) 
-    {
-        let row = pgcontent.insertRow();
-        for (key in course) 
-        {
-            let cell = row.insertCell();
-            let text = document.createTextNode(course[key]);
-            cell.appendChild(text);
-        }
-    }
+
+function getServerUrl(id){
+    return "https://csc205.cscprof.com/courses/" + id;
 }
-function removeColumns(courses)
-{
-    courses.forEach(function (course)
-    {
-        delete courses.Line;
-        delete course.openings;
-        delete course.building;
-        delete course.Capacity;
-        delete course.Campus;
-    });
-    return courses;
+
+
+function displayData(details) {
+
+
+    document.getElementById('Status').innerHTML = details.Status;
+    document.getElementById('Number').innerHTML = details.Number;
+    document.getElementById('Title').innerHTML = details.Title;
+    document.getElementById('Rating').innerHTML = details.Rating;
+    document.getElementById('Professor').innerHTML = details.Professor;
+    document.getElementById('Time').innerHTML = details.day + " "  + details.StartTime;
+    document.getElementById('Name').innerHTML = details.Department + " " + details.Number + " " + details.Section;
+    document.getElementById('Credits').innerHTML = details.Credits;
+    
+
+
+ 
 }
+
 
 
     
--- js/main.js
+++ js/main.js
@@ -1,26 +1,55 @@
 
-let courses= 
-[
-    {"Line":81,"Department":"BUS","Number":344,"Section":1,"Title":"MANAGEMENT OF INFORMATION SYSTEMS","Faculty":"Richards, Gordon P.","Openings":2,"Capacity":30,"Status":"Open","Day":"MWF","StartTime":"1:25:00 PM","EndTime":"2:20 PM","Campus":" Main Campus","Building":" Science and Engineering","Room":" SE 341 Computer Science Lab","Credits":3,"Start Date":"8\/30\/2021","End Date":"12\/17\/2021\r\n"}
-    ,{"Line":167,"Department":"CSC","Number":133,"Section":2,"Title":"SURVEY OF COMPUTER SCIENCE","Faculty":"Madeira, Scott","Openings":6,"Capacity":15,"Status":"Open","Day":"H","StartTime":"2:00:00 PM","EndTime":"4:50 PM","Campus":" Main Campus","Building":" Science and Engineering","Room":" SE 341 Computer Science Lab","Credits":0,"Start Date":"8\/30\/2021","End Date":"12\/17\/2021\r\n"}
-    ,{"Line":168,"Department":"CSC","Number":133,"Section":3,"Title":"SURVEY OF COMPUTER SCIENCE","Faculty":"Madeira, Scott","Openings":7,"Capacity":15,"Status":"Open","Day":"T","StartTime":"6:30:00 PM","EndTime":"9:20 PM","Campus":" Main Campus","Building":" Science and Engineering","Room":" SE 341 Computer Science Lab","Credits":0,"Start Date":"8\/30\/2021","End Date":"12\/17\/2021\r\n"}
-    ,{"Line":169,"Department":"CSC","Number":133,"Section":"0A","Title":"SURVEY OF COMPUTER SCIENCE","Faculty":"Richards, Gordon P.","Openings":15,"Capacity":45,"Status":"Open","Day":"TH","StartTime":"8:00:00 AM","EndTime":"9:20 AM","Campus":" Main Campus","Building":" Science and Engineering","Room":" SE 110 Chemistry room","Credits":4,"Start Date":"8\/30\/2021","End Date":"12\/17\/2021\r\n"}
-    ,{"Line":170,"Department":"CSC","Number":190,"Section":1,"Title":"HTML","Faculty":"Madeira, Scott","Openings":4,"Capacity":25,"Status":"Open","Day":"M","StartTime":"2:30:00 PM","EndTime":"3:25 PM","Campus":" Main Campus","Building":" Science and Engineering","Room":" SE 312A","Credits":1,"Start Date":"8\/30\/2021","End Date":"12\/17\/2021\r\n"}
-    ,{"Line":171,"Department":"CSC","Number":205,"Section":1,"Title":"HCI DESIGN & PROGRAMMING","Faculty":"Madeira, Scott","Openings":10,"Capacity":25,"Status":"Open","Day":"MWF","StartTime":"11:15:00 AM","EndTime":"12:10 PM","Campus":" Main Campus","Building":" Science and Engineering","Room":" SE 341 Computer Science Lab","Credits":3,"Start Date":"8\/30\/2021","End Date":"12\/17\/2021\r\n"}
-    ,{"Line":172,"Department":"CSC","Number":344,"Section":1,"Title":"MANAGEMENT INFORMATION SYSTEM","Faculty":"Poteete, Paul W. Steffine, Aaron","Openings":2,"Capacity":90,"Status":"Open","Day":"MWF","StartTime":"1:25:00 PM","EndTime":"2:20 PM","Campus":" Main Campus","Building":" Science and Engineering","Room":" SE 341 Computer Science Lab","Credits":3,"Start Date":"8\/30\/2021","End Date":"12\/17\/2021\r\n"}
-    ,{"Line":173,"Department":"CSC","Number":363,"Section":"E1","Title":"DATABASE SYSTEMS","Faculty":"Hinderliter, Jeffery A","Openings":4,"Capacity":30,"Status":"Open","Day":"T","StartTime":"6:30:00 PM","EndTime":"9:20 PM","Campus":" Main Campus","Building":" Science and Engineering","Room":" SE 233 Engineering Lab\/Classroom","Credits":3,"Start Date":"8\/30\/2021","End Date":"12\/17\/2021\r\n"}
-    ,{"Line":296,"Department":"HUM","Number":103,"Section":"0A","Title":"INVITATION TO THE HUMANTIES","Faculty":"Miller, Eric John","Openings":12,"Capacity":180,"Status":"Open","Day":"W","StartTime":"11:15:00 AM","EndTime":"12:10 PM","Campus":" Main Campus","Building":" Old Main","Room":" John White Chapel","Credits":0,"Start Date":"8\/30\/2021","End Date":"12\/17\/2021"}
-]
-window.onload = (event) => 
+console.log('Before event listeners');
+
+var etarget = "";
+    searchbox.addEventListener("input", (e) => etarget = e.target.value);
+    searchbox.oninput = function()
+    {
+        search();
+    }
+
+
+window.onload = (event) =>
 {
-    alert(courses[5].Title);
-    console.log('The page is loaded.We are in the consol');
-    let filteredData = removeColumns(courses);
+    fetch( "https://csc205.cscprof.com/courses")
+    .then(response => response.json())
+    .then(data => displayData(data)); 
+    
+
+}
+function displayData(results)
+{
+    course = results 
+    let filteredData = removeColumns(course);
+    
     let table = document.getElementById("course-table")
-    let data = Object.keys(filteredData [4]);
+    data = Object.keys(filteredData [0]);
     generatePageTitle(table, data);
     generatePage(table, filteredData);
+
 }
+function search()
+{
+    let tablebody = document.getElementById("course-data");
+    console.log(tablebody);
+    tablebody.remove();
+    
+    let someClasses = course.filter(oneClass => Object.keys(oneClass)
+            .some(key => String(oneClass[key]).toLowerCase().includes(etarget.toLowerCase())));
+
+    let table = document.getElementById("course-table") 
+    generatePage(table, someClasses);
+
+}
+searchbox.onfocus = ( ) => {document.getElementById("searchbox").classList.add("focus")}
+searchbox.onblur = ( ) => {document.getElementById("searchbox").classList.add("blur")}
+let classes = "";
+
+
+
+
+
+
 
 function generatePageTitle(table, data) 
 {
@@ -35,29 +64,54 @@ function generatePageTitle(table, data)
     }
 }
 
-function generatePage(table, data) 
+{function generatePage(table, data) 
 {
-    let pgcontent = table.createTBody();
+    let text = "";
+    let tablebody = table.createTBody();
+    tablebody.setAttribute("id", "course-data");
+
     for (let course of data) 
     {
-        let row = pgcontent.insertRow();
+        let row = tablebody.insertRow();
         for (key in course) 
         {
             let cell = row.insertCell();
-            let text = document.createTextNode(course[key]);
-            cell.appendChild(text);
+            if (key == "Title")
+            {
+                let link = '<a href="detailpage.html?id=' + course.id + '">' + course.Title + '</a>';
+                cell.innerHTML = link;
+            }
+            else if (key == "Email")
+            {
+                let link = '<a href= "mailto:notreal@geneva.edu' + course.id + '">' + course.Email + '</a>';
+                cell.innerHTML = link;
+            }
+            else 
+            {
+                cell.innerHTML = course[key];
+            }
+        
+        
         }
     }
 }
+
 
 function removeColumns(courses)
 {
     courses.forEach(function (course)
     {
+        let information = course.Department + " " + course.Number + " " + course.Section;
+        course.Section = information;
         delete course.openings;
         delete course.building;
         delete course.Capacity;
         delete course.Campus;
+
+        delete course.Number;
+        delete course.Department;
+
+       
     });
     return courses;
 }
@@ -65,4 +119,18 @@ function removeColumns(courses)
 
 
 
+} 
+//function Courses(info)  
+//{
+    //course = info;
+    //alert(course[170].Title);
+    //console.log('The page is loaded.We are in the consol');
+    //let filteredData = removeColumns(course);
+    //let table = document.getElementById("course-table")
+   // data = Object.keys(filteredData [4]);
+    //generatePageTitle(table, data);
+    //generatePage(table, filteredData);
     
+
+    
+//}
